32b5f15454a68932a4691da7cf9afad570ca193d
Ian Watts <ianwatts22@gmail.com>
2026-09-07 11:45:18 -0700

    fix: keep compact skill columns resizable

--- apps/MetagentMenuBar/Sources/InventorySection.swift
+++ apps/MetagentMenuBar/Sources/InventorySection.swift
@@ -612,7 +612,7 @@ struct InventorySection: View {
                         TableColumn(column.title, sortUsing: column.comparator) { row in
                             SkillTableColumnCell(column: column, row: row)
                         }
-                        .width(column.minWidth)
+                        .width(min: column.minWidth, ideal: column.minWidth)
                         .customizationID(column.id)
                         .defaultVisibility(column.defaultVisibility(selectedView))
                     }
@@ -631,7 +631,7 @@ struct InventorySection: View {
                         TableColumn(column.title, sortUsing: column.comparator) { row in
                             SkillTableColumnCell(column: column, row: row)
                         }
-                        .width(column.minWidth)
+                        .width(min: column.minWidth, ideal: column.minWidth)
                         .customizationID(column.id)
                         .defaultVisibility(column.defaultVisibility(selectedView))
                     }
